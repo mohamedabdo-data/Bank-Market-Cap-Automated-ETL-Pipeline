60b7e60ef72927c69e290c9bd9c3b61f8f637fa5
Mohamed Abdo <mohamabd315@gmail.com>
2026-07-30 11:50:18 +0300

    Refactor utils.py to add folder and file checks

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
-current_time()
+import os
+from datetime import datetime
 
-create_folder()
 
-check_file_exists()
+def current_time():
+    return datetime.now()
+
+
+def create_folder(folder):
+    os.makedirs(folder, exist_ok=True)
+
+
+def check_file_exists(path):
+    return os.path.exists(path)
