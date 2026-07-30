a4b0d658367864b3227cff019fcd10d06d3523f4
Mohamed Abdo <mohamabd315@gmail.com>
2026-07-30 11:50:57 +0300

    Define configuration constants in config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
-URL
-DATABASE_NAME
-CSV_FILE_NAME
-LOG_FILE
-EXCHANGE_RATE
+URL = "https://en.wikipedia.org/wiki/List_of_largest_banks"
+
+DATABASE_NAME = "data/bank_market_cap.db"
+
+CSV_FILE_NAME = "data/banks_market_cap.csv"
+
+LOG_FILE = "logs/pipeline.log"
+
+TABLE_NAME = "banks"
+
+EXCHANGE_RATE = 0.8
